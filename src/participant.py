4ff68f6b3a2e3887f37b00ded8bdb41b6eceda5a
Sebastian Musslick <sebastian@musslick.de>
2025-01-22 22:12:55 +0100

    fixed participant runner for now

--- src/participant.py
+++ src/participant.py
@@ -1,5 +1,8 @@
 import openai
 from dotenv import load_dotenv
+import os
+import numpy as np
+
 def generate(prompt):
     load_dotenv()
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -23,8 +26,28 @@ def execute_experiment(experiment):
     print(data)
     return data
 
+def execute_experiment_simple(trial_sequence, age, gender):
 
-import os
+    n_trials = len(trial_sequence)
+    rewards = np.zeros(n_trials)
+    choices = np.zeros(trial_sequence.shape)
+
+    prompt = ("You are a human participant with the following demographics: "
+              + str(age) + " years old and " + str(gender) + "."
+              + " You are participating in the following psychology experiment. ")
+    prompt += "In this experiment, you will see two boxes. Your goal is to find out which box gives more rewards by clicking on them. Each box might change how often it gives rewards, so keep experimenting and try to maximize your total rewards. When you select your response, answer only with '1' or '2' indicating the number of the bandit. "
+
+    for trial in range(n_trials):
+        prompt += "You see 2 bandits, Bandit 1 and Bandit 2. Choose a bandit by naming the number of the bandit. "
+        response = generate(prompt + "You name ")
+        llm_choice = int(response)
+        transformed_choice = llm_choice - 1
+        reward = int(trial_sequence[trial][transformed_choice])
+        choices[trial][transformed_choice] = 1
+        rewards[trial] = reward
+        prompt += "You chose bandit " + str(llm_choice) + ". You received a reward of " + str(reward) + ". "
+
+    return rewards, choices
 
 
 # Example usage
@@ -40,3 +63,8 @@ experiment = generate_experiment(trial_sequence)
 
 # Run the experiment on the synthetic participant
 data = execute_experiment(experiment)
+# rewards, choices = execute_experiment_simple(trial_sequence, "18", "male")
+
+# print(trial_sequence)
+print(rewards)
+print(choices)
